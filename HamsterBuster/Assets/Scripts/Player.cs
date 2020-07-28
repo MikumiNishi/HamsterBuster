d11976fb298bbb3039e6925a9c96bf4249873d81
ein0920 <c20026@std.it-college.ac.jp>
2020-07-29 03:29:12 +0900

    unityの追加

---
+++ HamsterBuster/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //const float Gravity = 9.81f;
+
+    //public float gravityScale = 1.0f;
+    public float VerticalSpeed = 1.0f;
+    public float HorizontalSpeed = 1.0f;
+    public Rigidbody Unity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Unity = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Vector3 vector = new Vector3();
+
+
+
+        float z = Input.GetAxis("Horizontal") * HorizontalSpeed;
+        float y = Input.GetAxis("Vertical") * VerticalSpeed;
+
+        Unity.AddForce(0, 0, z);
+        //Unity.AddTorque(0, y, 0);
+    }
+}
---
+++ HamsterBuster/Assets/Scripts/seedshooter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class seedshooter : MonoBehaviour
+{
+    public GameObject seedPrefab;
+    public float shotSpeed;
+    public float shotTorque;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) Shot();
+    }
+
+    public void Shot()
+    {
+        GameObject seed = (GameObject)Instantiate(
+            seedPrefab,
+            transform.position,
+            Quaternion.identity
+            );
+        Rigidbody seedRigidBody = seed.GetComponent<Rigidbody>();
+        seedRigidBody.AddForce(transform.forward * shotSpeed);
+        seedRigidBody.AddTorque(new Vector3(0, shotTorque, 0));
+    }
+}
