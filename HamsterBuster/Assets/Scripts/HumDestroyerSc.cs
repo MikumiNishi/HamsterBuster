a483862b0d2afdf43054c571c331757cd727088d
MikumiNishi <c20020@std.it-college.ac.jp>
2020-08-18 13:37:36 +0900

    Destroyerの調整

---
+++ HamsterBuster/Assets/Scripts/HumDestroyerSc.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumDestroyerSc : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+
+        // 画面外の壁に衝突したらhumsterタグのついたオブジェクトは削除
+        if (other.gameObject.tag == "humster")
+        {
+            Destroy(other.gameObject);
+
+            //BoxColliderコンポーネントを取得
+            BoxCollider component = this.gameObject.GetComponent<BoxCollider>();
+            // 指定したコンポーネントを削除
+            Destroy(component);
+
+        }
+    }
+}
--- HamsterBuster/Assets/Scripts/ScoreManagerSc.cs
+++ HamsterBuster/Assets/Scripts/ScoreManagerSc.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI; //UIの追加
 public class ScoreManagerSc : MonoBehaviour
 {
     public GameObject score_object = null; //textオブジェクト
+    public Text scoreText;
     public int score_num = 0; //スコア変数
 
 
@@ -13,19 +14,44 @@ public class ScoreManagerSc : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        score_num = 0;
+        SetScore();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        //オブジェクトからTextコンポーネントを取得
-        Text score_text = score_object.GetComponent<Text>();
-        //テキストの表示を入れ替える
-        score_text.text = "Score : " + score_num;
-        
-        score_num += 1; //とりあえず１加算し続ける
+        string yourTag = collision.gameObject.tag;
 
-        
+        if (yourTag == "humster")
+        {
+            score_num += 150;
+        }
+        else
+        {
+            score_num = 0;
+        }
+
+        SetScore();
+
     }
+
+    void SetScore()
+    {
+        scoreText.text = string.Format("Score:{0}", score_num);
+
+    }
+
+
+    //ここいる？↓
+    private void OnGUI()
+    {
+        GUI.color = Color.black;
+
+        //現在のスコアを表示
+        string label = "Score : " + score_num;
+
+        GUI.Label(new Rect(0, 0, 100, 30), label);
+    }
+
 }
