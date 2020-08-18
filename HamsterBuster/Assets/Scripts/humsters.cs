d5d20174d746fc339e8f8d6f29237ca2671f90f7
MikumiNishi <c20020@std.it-college.ac.jp>
2020-08-18 11:39:42 +0900

    ハムスターの向きの調整のスクリプトコピペ

--- HamsterBuster/Assets/Scripts/humsters.cs
+++ HamsterBuster/Assets/Scripts/humsters.cs
@@ -39,15 +39,16 @@ public class humsters : MonoBehaviour
 			Appearhumster();
 		}
 	}
-		//　敵出現メソッド
-		void Appearhumster()
+	//　敵出現メソッド
+	void Appearhumster()
 	{
 		//　出現させる敵をランダムに選ぶ
 		var randomValue = Random.Range(0, humster.Length);
 		//　敵の向きをランダムに決定
-		var randomRotationZ= Random.value * 90f;
+		var RotationZ = 90f;
 
-		GameObject.Instantiate(humster[randomValue], transform.position, Quaternion.Euler(0f, randomRotationZ, 0f));
+
+		GameObject.Instantiate(humster[randomValue], transform.position, Quaternion.Euler(0f, RotationZ, 0f));
 
 		numberOfhumster++;
 		elapsedTime = 0f;
