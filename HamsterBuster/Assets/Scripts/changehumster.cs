fa939991057497a06ef52cc7f2a2eaa15c21472b
ein0920 <c20026@std.it-college.ac.jp>
2020-08-20 14:11:39 +0900

    PG中

---
+++ HamsterBuster/Assets/Scripts/changehumster.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changehumster : MonoBehaviour
+{
+    public GameObject[] angly_HumsterPrefab;
+    public GameObject[] Happy_HumsterPrefab;
+    
+    void OnOnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "seed")
+        {
+            GetComponent<Prefab>().GameObject.angly_HumsterPrefab= GameObject.Happy_HumsterPrefab;
+        }
+    }
+}
